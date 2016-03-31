a772fd7f0968c946c38b844cc79c86d2c9637919
dave glynn <dave@ocl.ie>
2016-03-31 21:49:37 +0100

    final todos

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true&q=work
 app.get('/todos',function (req, res) {
  	var queryParams = req.query;
  	var filteredTodos = todos;
@@ -25,8 +25,15 @@ app.get('/todos',function (req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: false});
  	}
 
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length> 0)  {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+				return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		})
+	}
+	
 	res.json(filteredTodos);
 });
+
 
 // GET /todos/:id
 app.get('/todos/:id',function (req, res) {
