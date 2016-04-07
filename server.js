e88724502fa09dcdba3c84637b00eda816a6155c
dave glynn <dave@ocl.ie>
2016-04-07 09:11:45 +0100

    Adding DELETE / Users/login

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
-		req.user.addTodo(todo).then(function(){
+		req.user.addTodo(todo).then(function() {
 			return todo.reload();
 		}).then(function(todo) {
 			res.json(todo.toJSON());
@@ -128,7 +128,7 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 			id: todoId,
 			userId: req.user.get('id')
 		}
-	}).then(function(todo) { 
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -158,22 +158,33 @@ app.post('/users', function(req, res) {
 });
 
 //POST /users/login
-app.post('/users/login', function (req, res) {
+app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
-	db.user.authenticate(body).then(function (user) {
+	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
+		return db.token.create({
+			token: token
+		});
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());	
-		} else {
-			res.status(401).send();
-		}
-	}, function () {
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function() {
 		res.status(401).send();
 	});
 });
 
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
+	});
+});
 
 db.sequelize.sync({
 	force: true
